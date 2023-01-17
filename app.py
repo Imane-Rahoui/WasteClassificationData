cf1a6f56965b0d711d66b8d9e459819497c4a52a
Imane-Rahoui <Imaanerahoui@gmail.com>
2023-01-17 21:43:39 +0100

    comments added

--- app.py
+++ app.py
@@ -6,56 +6,70 @@ import tensorflow as tf
 from keras.models import load_model
 import warnings
 
+#All warnings are ignored
 warnings.filterwarnings("ignore")
 
-from flask import Flask, redirect, url_for, request, render_template
+from flask import Flask, request, render_template
 from werkzeug.utils import secure_filename
 
-# Define a flask app
 app = Flask(__name__)
 
-# Model saved with Keras model.save()
 MODEL_PATH = 'models/best_model.hdf5'
 
-# Load your trained model
+# Load model
 model = load_model(MODEL_PATH)
 
 
-def model_predict(img_path, model):
-    img1 = tf.keras.utils.load_img(img_path, target_size=(155, 155))
-    img1 = tf.keras.utils.img_to_array(img1)
-    img1 = np.expand_dims(img1, axis=0)
+def predictOR(img_path, model):
 
-    result = model.predict(img1 / 255)
+    #print("f1")
+    # recuperer l'image et ajuster
+    image = tf.keras.utils.load_img(img_path, target_size=(155, 155))
+
+    #print("avant : ")
+    #print(image)
+    # convert image to numpy array
+    image = tf.keras.utils.img_to_array(image)
+
+    # ajouter un nouvel axe au tableau.
+    image = np.expand_dims(image, axis=0)
+    print("apres : ")
+    #print(image)
+    result = model.predict(image / 255)
 
     return result
 
 
 @app.route('/', methods=['GET'])
 def Result():
-    # Main page
+    print("f2")
     return render_template('result.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
+    print("f3")
     if request.method == 'POST':
-        # Get the file from post request
+        # recuperer l'image
         f = request.files['file']
 
-        # Save the file to ./uploads
+        print("fichier : "+__file__)
+        # Recuperer le rep du projet
         basepath = os.path.dirname(__file__)
+        # Enregister f DIR
         file_path = os.path.join(
             basepath, 'DIR', secure_filename(f.filename))
         f.save(file_path)
-
-        # Make prediction
-        preds = model_predict(file_path, model)
+        print("allo")
+        preds = predictOR(file_path, model)
         print("-------------- " + format(preds[0][1] * 100, '.2f'))
         print("-------------- " + format(preds[0][0] * 100, '.2f'))
 
-        # Process your result for human
-        if preds[0][0] < preds[0][1]:  #
+        # first one organic
+        # second one recyclable
+        # ----------------------------- QUEEEESTTTT
+        print(preds)
+        if preds[0][0] < preds[0][1]:
             return format(preds[0][1] * 100, '.1f') + "% Recyclable"
         else:
             return format(preds[0][0] * 100, '.1f') + "% Organic"
